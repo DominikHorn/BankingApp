77e58fa1f786ba3b16de84bd171d81ffaf89d899
Dominik Horn <horn.dominik101@yahoo.de>
2018-12-02 00:43:45 +0100

    Enhance OriginPlot by adding piechart

--- src/components/plots/OriginPlot.tsx
+++ src/components/plots/OriginPlot.tsx
@@ -3,29 +3,37 @@ import * as React from 'react';
 import {
     Bar,
     BarChart,
-    CartesianGrid,
+    CartesianGrid, Cell, Pie, PieChart, ReferenceLine,
     ResponsiveContainer,
-    Tooltip,
-    XAxis,
+    Tooltip, XAxis,
     YAxis
 } from "recharts";
 import {ICsvEntity} from "../../types/ICsvEntity";
 import {Money} from "../../types/Money";
+import './Plots.css'
 
 export interface IOriginPlotProps {
     transactionData: ICsvEntity[],
 }
 
-interface IOriginPlotDataPoint {
+// TODO: value should be Money
+interface IOriginPlotBarDataPoint {
     payee: string;
     value: number;
     valuePos?: number;
     valueNeg?: number;
 }
 
+// TODO: value should be Money!
+interface IOriginPlotPieDataPoint {
+    name: string;
+    value: number;
+    color: string;
+}
+
 export class OriginPlot extends React.PureComponent<IOriginPlotProps, any> {
-    /** Data for this plot */
-    private plotData = memoize(
+    /** Data for the bar plot */
+    private barPlotData = memoize(
         (transactionData: ICsvEntity[]) => {
             const map = {};
             for (const entity of transactionData) {
@@ -33,15 +41,18 @@ export class OriginPlot extends React.PureComponent<IOriginPlotProps, any> {
                 map[entity.payee] = Money.add(current, entity.amount);
             }
 
-            const result = [];
+            const result: IOriginPlotBarDataPoint[] = [];
             // TODO rewrite code to avoid this for ... in ...
             // tslint:disable-next-line
             for (const payee in map) {
-                const row: IOriginPlotDataPoint = {
+                const row: IOriginPlotBarDataPoint = {
                     payee,
                     value: map[payee].value(),
                 };
 
+                if (row.value === 0) {
+                    continue;
+                }
                 if (row.value < 0) {
                     row.valueNeg = row.value;
                 } else {
@@ -67,26 +78,83 @@ export class OriginPlot extends React.PureComponent<IOriginPlotProps, any> {
         }
     );
 
+    /** Data for the pie plot */
+    private piePlotData = memoize(
+        (barPlotData: IOriginPlotBarDataPoint[]) => {
+            // Take biggest x contributers, aggregate rest into 'other' category
+            const aggrAfter = 5;
+            const result: IOriginPlotPieDataPoint[] = barPlotData
+                .slice(0, aggrAfter)
+                .map(dp => ({
+                    color: dp.valuePos ? '#00cc00' : '#ff6666',
+                    name: dp.payee,
+                    value: dp.value < 0 ? -dp.value : dp.value,
+                }));
+
+            if (barPlotData.length > aggrAfter) {
+                const aggr = {name: 'Other', value: 0, color: "#AAAAAA"};
+                for (let i = 7; i < barPlotData.length; i++) {
+                    const val = barPlotData[i].value;
+                    aggr.value += val > 0 ? val : -val;
+                }
+                aggr.value = parseFloat(aggr.value.toFixed(2));
+                result.push(aggr);
+            }
+
+
+            return result;
+        }
+    );
+
     public render() {
-        const plotData = this.plotData(this.props.transactionData);
+        const barPlotData = this.barPlotData(this.props.transactionData);
+        const piePlotData = this.piePlotData(barPlotData);
+
+        // tslint:disable-next-line
+        console.table(piePlotData);
 
         return (
-            <ResponsiveContainer
-                width={"100%"}
-                height={400}
-            >
-                <BarChart
-                    data={plotData}
-                    margin={{top: 20, right: 50, bottom: 100}}
+            <div>
+                <ResponsiveContainer
+                    width={"75%"}
+                    height={400}
+                    className="InlineBlock"
                 >
-                    <CartesianGrid strokeDasharray="1 1"/>
-                    <XAxis dataKey="payee" interval={0} tick={{angle: 20, textAnchor: "start"}}/>
-                    <YAxis/>
-                    <Tooltip/>
-                    <Bar dataKey="valuePos" fill="green" stackId="stack"/>
-                    <Bar dataKey="valueNeg" fill="red" stackId="stack"/>
-                </BarChart>
-            </ResponsiveContainer>
+                    <BarChart
+                        data={barPlotData}
+                        margin={{top: 20, right: 50}}
+                    >
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <XAxis dataKey="payee"/>
+                        <YAxis/>
+                        <Tooltip/>
+                        <ReferenceLine y={0} label="" stroke="#000"/>
+                        <Bar dataKey="valuePos" fill="#00cc00" stackId="stack"/>
+                        <Bar dataKey="valueNeg" fill="#ff6666" stackId="stack"/>
+                    </BarChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer
+                    width={"25%"}
+                    height={400}
+                    className="InlineBlock"
+                >
+                    <PieChart
+                        margin={{top: 20, right: 50}}
+                    >
+                        <Tooltip/>
+                        <Pie
+                            data={piePlotData}
+                            dataKey="value"
+                            fill="#8884d8"
+                            label={true}
+                            innerRadius={60}
+                            outerRadius={100}
+                        >
+                            {piePlotData.map((dp) => <Cell key={dp.name} fill={dp.color}/>)}
+                        </Pie>
+                    </PieChart>
+                </ResponsiveContainer>
+            </div>
         );
     }
 }
